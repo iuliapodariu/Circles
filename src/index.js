76bdaad4a4a0e616797b0e46343d725214deba1a
iuliapodariu <72615228+iuliapodariu@users.noreply.github.com>
2021-07-09 17:56:27 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function click1() {
+  alert("da");
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+  ctx.beginPath();
+  ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+  ctx.stroke();
+}
